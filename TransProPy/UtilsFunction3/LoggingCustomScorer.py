d9ab4a90430af42f5c07b66da2167a4865b8d402
SSSYDYSSS <yudongyue@mail.nankai.edu.cn>
2023-12-22 00:07:21 +0800

    somechange

--- TransProPy/UtilsFunction3/LoggingCustomScorer.py
+++ TransProPy/UtilsFunction3/LoggingCustomScorer.py
@@ -1,22 +1,26 @@
 from sklearn.metrics import accuracy_score
 import logging
+import time
 
 def logging_custom_scorer(n_iter=10, n_cv=5):
     """
     Creates a custom scorer function for use in model evaluation processes.
-    This scorer logs the accuracy score each time it is called.
+    This scorer logs both the accuracy score and the time taken for each call.
 
     Parameters:
     n_iter (int): Number of iterations for the search process. Default is 10.
     n_cv (int): Number of cross-validation splits. Default is 5.
 
     Returns:
-    function: A custom scorer function that logs the accuracy score each time it is called.
+    function: A custom scorer function that logs the accuracy score and time taken for each call.
     """
+
+    # Initialize the time for the first call
+    last_time = time.time()
 
     def custom_scorer(y_true, y_pred):
         """
-        Inner function to calculate the accuracy score and log it.
+        Inner function to calculate the accuracy score, log it, and measure the time taken.
 
         Parameters:
         y_true (array-like): True labels.
@@ -25,10 +29,20 @@ def logging_custom_scorer(n_iter=10, n_cv=5):
         Returns:
         float: The accuracy score.
         """
+        nonlocal last_time  # Reference the last_time from the outer scope
+
+        # Record the current time and calculate the elapsed time since the last call
+        current_time = time.time()
+        elapsed = current_time - last_time
+        last_time = current_time  # Update last_time for the next call
+
         # Calculate the accuracy score
         score = accuracy_score(y_true, y_pred)
-        # Log the score
-        logging.info(f"One scoring iteration completed, accuracy: {score}")
+
+        # Log the accuracy and the time taken for this scoring iteration
+        logging.info(f"One scoring iteration completed, accuracy: {score}, time taken: {elapsed:.2f} seconds")
+
         return score
 
     return custom_scorer
+
--- TransProPy/UtilsFunction3/SetupLoggingAndProgressBar.py
+++ TransProPy/UtilsFunction3/SetupLoggingAndProgressBar.py
@@ -13,11 +13,24 @@ def setup_logging_and_progress_bar(n_iter, n_cv):
     tqdm object: An initialized tqdm progress bar.
     """
 
-    # Configure logging
+    # Configure basic logging - this time, without filename and filemode
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s: %(message)s',
-                        filename='progress.log',
-                        filemode='w')
+                        format='%(asctime)s - %(levelname)s: %(message)s')
+
+    # Create a file handler for logging to a file
+    file_handler = logging.FileHandler('progress.log', mode='w')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(message)s'))
+
+    # Create a stream handler for logging to the console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(message)s'))
+
+    # Get the default logger and add the two handlers to it
+    logger = logging.getLogger()
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
 
     # Calculate total iterations
     total_iterations = n_iter * n_cv
@@ -25,4 +38,3 @@ def setup_logging_and_progress_bar(n_iter, n_cv):
     # Initialize and return tqdm progress bar
     pbar = tqdm(total=total_iterations, desc='RandomizedSearchCV Progress')
     return pbar
-
